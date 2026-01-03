3a03202baeb379f409b8eae517fa957b57572926
David-Apamo <davidapamo2019@gmail.com>
2026-01-04 00:59:32 +0100

    second backend commit2

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT || 5432,
+// });
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 module.exports = pool;
